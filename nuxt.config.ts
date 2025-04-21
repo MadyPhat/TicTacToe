76d495aa49d7e8dc88b16a4a80504e785eb07a2a
MadyPhat <phatmady21@gmail.com>
2025-04-21 13:58:44 +0700

    Add build to github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['nuxt-particles'],
+  // app: {
+  //   baseURL: '/TicTacToe/'
+  // },
+  ssr: false,
 })
